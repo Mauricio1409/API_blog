478d843cb5f99b3176a3a4d4d79d596c2f0667ca
Mauricio Torti <mauriciotorti1@gmail.com>
2025-11-30 19:30:24 -0300

    Agregado de conexion a db de postgress

--- core/settings.py
+++ core/settings.py
@@ -71,8 +71,12 @@ ASGI_APPLICATION = 'core.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'django',
+        'PASSWORD': 'postgres',
+        'HOST': 'django_db_blog',
+        'PORT': 5432,
     }
 }
 
